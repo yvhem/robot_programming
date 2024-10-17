e96d6200aacd32fe1f1de9db2228d71fa7208481
lil-fe <tranzocchi.1891909@studenti.uniroma1.it>
2024-10-17 15:44:27 +0200

    add ROS2 publisher-listener example

---
+++ in_class/ros2_ws/build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fe/OneDrive/Università/AIRO/Robot Programming/robot_programming/in_class/ros2_ws/src/cpp_pubsub
+# Build directory: /home/fe/OneDrive/Università/AIRO/Robot Programming/robot_programming/in_class/ros2_ws/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
